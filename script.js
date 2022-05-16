eeb71171779cabd51ecdac6b62b36948913ec4ab
Thais Moreira <thaismoreira@alunos.utfpr.edu.br>
2022-05-15 21:00:17 -0300

    modificacoes - busca, login, validacao campos

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-function login() {
-  const params = {
-    email: document.getElementById('email').value,
-    password: document.getElementById('password').value
-  };
+function logado() {
+  if(getToken()){
+    document.querySelector('#loggedout').style.visibility = 'hidden';
+    document.querySelector('#loggedin').style.visibility = 'visible';
+  }
+}
 
-  axios.post('https://reqres.in/api/login', params)
-    .then(res => {
-      const { token } = res.data;
-      localStorage.setItem('token', token);
-    });
+function login() {
+  var valida = false;
+  if (document.getElementById('email').value.length < 3) {
+    document.getElementById('validacao').innerHTML = 'Email invalido';
+  }else if (document.getElementById('password').value.length < 3){
+    document.getElementById('validacao').innerHTML = 'Senha invalida';
+  }else{
+    valida = true;
+  }
+  if (valida === true){
+    const params = {
+      email: document.getElementById('email').value,
+      password: document.getElementById('password').value
+    };
+
+    axios.post('https://reqres.in/api/login', params)
+      .then(res => {
+        const { token } = res.data;
+        localStorage.setItem('token', token);
+        document.querySelector('#loggedout').style.visibility = 'hidden';
+        document.querySelector('#loggedin').style.visibility = 'visible';
+
+      })
+      .catch(error => {
+        document.getElementById('validacao').innerHTML = 'Usuário não encontrado';
+      });
+  }
 }
 
 function getToken() {
@@ -17,6 +40,7 @@ function getToken() {
 
 function logout() {
   localStorage.clear();
+  location.reload();
 }
 
 function search() {
@@ -25,6 +49,7 @@ function search() {
 
   axios.get(`https://api.social-searcher.com/v2/search?q=${search}&network=twitter&key=11a91cc912fbd30f9dfdd8bff39dcaef&page=0&limit=10`)
     .then(res => {
+      postList.innerHTML = "";
       const { posts } = res.data;
       for (post of posts) {
         const item = document.createElement('li');
